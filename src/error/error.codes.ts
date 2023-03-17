44cc9e4539f490e47529ba78b9a927f231069f56
ezraRubio <ezra@ezrarubio.com>
2023-03-17 14:21:22 +0200

    implemented error handling

---
+++ src/error/error.codes.ts
@@ -0,0 +1,6 @@
+export enum ErrorCodes {
+    UNPROCESSABLE_DATA = "error/unprocessable_data",
+    INVALID_ENTRY = "error/invalid_entry",
+    INTERNAL_SERVER_ERROR = "error/internal_error",
+    DUPLICATE_DATA = "error/duplicate_entry",
+}
---
+++ src/error/http.error.handler.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import { ErrorCodes } from "./error.codes";
+import { AppError } from "./error.module";
+
+interface ErrorHandler {
+  (
+    error: Error | AppError,
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ): void;
+}
+
+export const httpErrorHandler: ErrorHandler = (
+  error: AppError,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const status = (error as AppError)?.status || 500;
+  const { code, details, errors } = error;
+
+  const res = {
+    code: code || ErrorCodes.INTERNAL_SERVER_ERROR,
+    details: details,
+    errors: errors || undefined
+  };
+
+  response.status(status).json(res);
+};
