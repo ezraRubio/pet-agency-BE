524e226ce0323884f2ecdadb12db3ec439f350c1
ezraRubio <ezra@ezrarubio.com>
2023-03-21 20:31:40 +0200

    add validations to pet routes

--- src/pet/pet.controller.ts
+++ src/pet/pet.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { Pet } from "./pet.model";
+import { addPetValidator, editPetValidator } from "./pet.request.validator";
 import { PetService } from "./pet.service";
 
 const pet = "/pet";
@@ -7,19 +8,37 @@ const petById = "/pet/:id";
 
 export class PetController {
   router = Router();
-//TODO: make validations 
+
   constructor(private petService: PetService) {
-    this.router.get(petById, (req: Request, res: Response, next: NextFunction) =>
-      this.getPetById(req.params.id).then((pet) => pet).catch(err=> next(err))
+    this.router.get(
+      petById,
+      (req: Request, res: Response, next: NextFunction) =>
+        this.getPetById(req.params.id)
+          .then((pet) => pet)
+          .catch((err) => next(err))
     );
-    this.router.post(pet, (req: Request, res: Response, next: NextFunction) =>
-      this.addNewPet(req.body).then((pet) => pet).catch(err=> next(err))
+    this.router.post(
+      pet,
+      addPetValidator,
+      (req: Request, res: Response, next: NextFunction) =>
+        this.addNewPet(req.body)
+          .then((pet) => pet)
+          .catch((err) => next(err))
     );
-    this.router.put(petById, (req: Request, res: Response, next: NextFunction) =>
-      this.editExistingPet(req.params.id, req.body).then((pet) => pet).catch(err=> next(err))
+    this.router.put(
+      petById,
+      editPetValidator,
+      (req: Request, res: Response, next: NextFunction) =>
+        this.editExistingPet(req.params.id, req.body)
+          .then((pet) => pet)
+          .catch((err) => next(err))
     );
-    this.router.delete(petById, (req: Request, res: Response, next: NextFunction) =>
-      this.deleteExistingPet(req.params.id).then((succeed) => succeed).catch(err=> next(err))
+    this.router.delete(
+      petById,
+      (req: Request, res: Response, next: NextFunction) =>
+        this.deleteExistingPet(req.params.id)
+          .then((succeed) => succeed)
+          .catch((err) => next(err))
     );
   }
 
--- src/pet/pet.request.validator.ts
+++ src/pet/pet.request.validator.ts
@@ -1,5 +1,7 @@
 import { ErrorCodes } from "../error/error.codes";
 import { validatorFactory } from "../utils/validator.utils";
-import { searchSchema } from "./pet.request";
+import { addNewPetSchema, editPetSchema, searchSchema } from "./pet.request";
 
 export const searchQueryValidator = validatorFactory(ErrorCodes.INVALID_ENTRY, searchSchema)
+export const addPetValidator = validatorFactory(ErrorCodes.INVALID_ENTRY, addNewPetSchema)
+export const editPetValidator = validatorFactory(ErrorCodes.INVALID_ENTRY, editPetSchema)
