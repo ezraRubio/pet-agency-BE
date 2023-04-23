f4daceebcec0fff63c940354b86d4229c212aca4
Ezra <81970147+ezraRubio@users.noreply.github.com>
2023-04-23 20:50:05 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-import config from './config';
 import {  UnprotectedControllers, ProtectedControllers } from './context';
 import { httpErrorHandler } from './error/http.error.handler';
 import { auth } from './auth/auth.middleware';
 
 export const app = express()
-  .use(cors({ origin: config.ALLOWED_ORIGINS }))
+  .use(cors())
   .enable("trust proxy")
   .disable('x-powered-by')
   .use(express.json())
